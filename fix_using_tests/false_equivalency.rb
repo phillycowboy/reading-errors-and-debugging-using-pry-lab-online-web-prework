72831ac383d8060aa4bc0921289182ffe974ff39
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-22 15:51:00 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,9 +12,10 @@ end
 
 def selection(num)
   if num = 1
+    binding.pry
      "YUM YUM MUNCH MUNCH MUNCH"
-     binding.pry
   elsif num = 2
+    binding.pry
      "HAM HAM HAM IN MY TUMMY"
 
   end
