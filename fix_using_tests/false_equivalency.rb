6c78c68f406a9d4a0130bdaa7ffd594823ee81f7
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-22 16:23:47 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,8 +11,8 @@ def prompt_user
 end
 
 def selection(num)
-  # if num = 1
-  #    "YUM YUM MUNCH MUNCH MUNCH"
+  if num = 1
+     "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
      "HAM HAM HAM IN MY TUMMY"
 
