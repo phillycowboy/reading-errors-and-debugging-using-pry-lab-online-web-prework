726d11fbf25c8fc11aff5c9aac5a42fe51b1ae41
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-22 16:22:20 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,13 +12,9 @@ end
 
 def selection(num)
   if num = 1
-    "HAM HAM HAM IN MY TUMMY"
-
-    #  "YUM YUM MUNCH MUNCH MUNCH"
+     "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-    "YUM YUM MUNCH MUNCH MUNCH"
-
-    #  "HAM HAM HAM IN MY TUMMY"
+     "HAM HAM HAM IN MY TUMMY"
 
   end
 end
